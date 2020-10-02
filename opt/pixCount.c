ec893d8b20ed8022395bd93fc0de553b37700e29
Abheek Saha <abheek.saha@gmail.com>
2020-10-02 15:39:59 +0530

    Pixel counting based on ranges

--- opt/pixCount.c
+++ opt/pixCount.c
@@ -27,52 +27,50 @@ int makeRanges(uint32_t colour, int *lowR, int *highR, int *lowG, int *highG, in
 uint32_t rgbToInt(char *) ;
 
 uint32_t qam16 = 0xffff00, qam64=0xff0000, qam256=0x0000ff ;
+typedef struct {
+	char name[1024] ;
+	uint32_t colour ;
+} range_t ;
+
+#define MAXRANGES 24
+
+range_t ranges[MAXRANGES] ;
+int rangeCount = 24 ;
+
+int processRanges(char *rf,range_t * ranges) ;
 int main(int argc, char* argv[])
 {
 	int notExit = 1 ;
-	char imageName[1024] ;
+	char rangeFile[1024] ;
 	char ch ;
 	extern char *optarg ;
+	extern int optind ;
 	int option_index = 0;
-	bzero(imageName,1024*sizeof(char)) ;
+	bzero(rangeFile,1024*sizeof(char)) ;
 	int variation = 10;
+    	Mat frame, frame_RGB, frame_threshold;
+	int i;
 
 	static struct option long_options[] = {
-                   {"qam16",    required_argument, 0,  4 },
-                   {"qam64",  	required_argument, 0,  6 },
-                   {"qam256",  required_argument, 0,  8 },
-                   {"var",  required_argument, 0,  'v' },
+                   {"rangefile",  required_argument, 0,  'r' },
+                   {"variation",  required_argument, 0,  'v' },
                    {0,         0,                 0,  0 }
 	};
-	while ((ch = getopt_long(argc, argv, "f:", long_options, &option_index)) != -1)
+	while ((ch = getopt_long(argc, argv, "r:v:", long_options, &option_index)) != -1)
 	{
                switch (ch) {
-		case 'f': strncpy(imageName,optarg,1023) ; break ;
 		case 'v': variation = atoi(optarg) ; break ;
-               	case 4: qam16 = rgbToInt(optarg) ; break ;
-               	case 6: qam64 = rgbToInt(optarg) ; break ;
-               	case 8: qam256 = rgbToInt(optarg) ; break ;
+		case 'r': strncpy(rangeFile,optarg,1023) ; break ;
                default: printf("?? getopt returned character code 0%o ??\n", ch);
                }
 	}
-	printf("16QAM:%x 64QAM:%x 256QAM:%x\n",qam16,qam64,qam256) ;
 
-
-
-           if (optind < argc) {
-
-               printf("non-option ARGV-elements: ");
-
-               while (optind < argc)
-
-                   printf("%s ", argv[optind++]);
-
-               printf("\n");
-
-           }
-    
+	rangeCount = processRanges(rangeFile,ranges) ;
+	printf("Discovered %d ranges\n", rangeCount) ;
 	namedWindow(window_detection_name);
-    Mat frame, frame_RGB, frame_threshold;
+	while (optind < argc) {
+	char *imageName = argv[optind++] ;
+	printf("Processing %s: ",imageName) ;
 	if (strlen(imageName) == 0) {
 		fprintf(stderr,"Need an image name\n" ) ;
 		exit(1) ;
@@ -86,60 +84,27 @@ int main(int argc, char* argv[])
 	frame = imread(imageName) ;
     {
 
-        // Convert from BGR to HSV colorspace
-
-        //cvtColor(frame, frame_HSV, COLOR_BGR2HSV);
-
-        // Detect the object based on HSV Range Values
-
-	//    low_B = 240; high_B = 255 ; low_G = 240; high_G = 255 ; low_R = 240; high_R = 255 ;
-        //inRange(frame, Scalar(low_B, low_G, low_R), Scalar(high_B, high_G, high_R), frame_threshold);
 	int coloured = convertWhiteToBlack(frame) ;
 	printf("Counted %d coloured\n", coloured) ;
-	    // First 16qam.
-	printf("QAM 16:\n") ;
-	makeRanges(qam16,&low_R, &high_R, &low_G, & high_G, &low_B, &high_B,variation) ;
+
+	for (i=0; i<rangeCount; i++) {
+		printf("%d:%s=>\t", i, ranges[i].name) ;
+		makeRanges(ranges[i].colour,&low_R, &high_R, &low_G, & high_G, &low_B, &high_B,variation) ;
 //	    low_B = 0; high_B = 1 ; low_G = 0; high_G = 1 ; low_R = 0; high_R = 255 ;
-        inRange(frame, Scalar(low_B, low_G, low_R), Scalar(high_B, high_G, high_R), frame_threshold);
-        imshow(window_detection_name, frame_threshold);
-	countPixels(frame_threshold,coloured) ;
-	waitKey(5000) ;
-		// Then Blue
-	printf("QAM 64:\n") ;
-	makeRanges(qam64,&low_R, &high_R, &low_G, & high_G, &low_B, &high_B,variation) ;
-//	    low_B = 128; high_B = 255 ; low_G = 0; high_G = 1 ; low_R = 0; high_R = 1 ;
-        inRange(frame, Scalar(low_B, low_G, low_R), Scalar(high_B, high_G, high_R), frame_threshold);
-        imshow(window_detection_name, frame_threshold);
-	countPixels(frame_threshold,coloured) ;
-	waitKey(5000) ;
-		// Then Yellow
-	printf("QAM 256:\n") ;
-	makeRanges(qam256,&low_R, &high_R, &low_G, & high_G, &low_B, &high_B,variation) ;
-//	    low_B = 0; high_B = 255 ; low_G = 240; high_G = 255 ; low_R = 240; high_R = 255 ;
-        inRange(frame, Scalar(low_B, low_G, low_R), Scalar(high_B, high_G, high_R), frame_threshold);
-	countPixels(frame_threshold,coloured) ;
-        imshow(window_detection_name, frame_threshold);
-
-        // Show the frames
-
-//        imshow(window_capture_name, frame);
-        //imshow(window_detection_name, frame_threshold);
-
-        char key = (char) waitKey(-1);
-
-        if (key == 'q' || key == 27)
-
-        {
-
-            exit(3);
-
-        }
-
+        	inRange(frame, Scalar(low_B, low_G, low_R), Scalar(high_B, high_G, high_R), frame_threshold);
+        	imshow(window_detection_name, frame_threshold);
+		countPixels(frame_threshold,coloured) ;
+		char key = waitKey(500) ;
+        	if (key == 'q' || key == 27)
+        	{
+            		exit(3);
+        	}
+	}
     }
-
+	}
     return 0;
-
 }
+
 
 int makeRanges( uint32_t col,
 		int *lowR, int *highR,
@@ -153,10 +118,12 @@ int makeRanges( uint32_t col,
 	*highG = (col>>8 & 0xff) + var/2 ; if (*highG > 255) *highG = 255 ;
 	*lowB = (col & 0xff) - var/2 ; if (*lowB < 0) *lowB = 0 ;
 	*highB = (col & 0xff) + var/2 ; if (*highB > 255) *highB = 255 ;
+#if 0
 	printf ("Range: Red: %d to %d, Green: %d to %d, Blue:%d to %d\n",
 			*lowR, *highR,
 			*lowG, *highG,
 			*lowB, *highB) ;
+#endif
 }
 
 typedef cv::Point3_<uint8_t> Pixel;
@@ -190,7 +157,7 @@ int countPixels(Mat frame,int coloured)
 	int rows=frame.rows ;
 	int cols=frame.cols ;
 	int dim = rows*cols ;
-	printf("Dimension:rows=%d coluns=%d\n",frame.rows, frame.cols) ;
+	printf("Dimension:rows=%d coluns=%d\t",frame.rows, frame.cols) ;
 	printf("Number of channels:%d depth=%d, ",frame.channels(),frame.depth()) ;
 	// first. raw pointer access.
 #if 1
@@ -217,6 +184,35 @@ uint32_t rgbToInt(char *rgbname)
 	tok = strtok(rgbname,",") ; red = atoi(tok) ;
 	tok = strtok(NULL,",") ; green = atoi(tok) ;
 	tok = strtok(NULL,",\n") ; blue = atoi(tok);
-	printf("%x,%x,%x\n",red,green,blue) ;
 	return (red<<16 | green<<8 | blue) ;
 }
+
+int processRanges(char *rfile, range_t *r)
+{
+	int rp = 0 ;
+	FILE *rf = NULL ;
+	char finf[1024] ;
+	int flen=0;
+	if ((rf = fopen(rfile,"r")) == NULL) {
+		fprintf(stderr,"Couldn't open %s\n",rfile) ;
+		perror("File read error\n") ;
+	}
+	while (fgets(finf,1023,rf) != 0) {
+		int red,green,blue ;
+		char *tok = strtok(finf," ,\t\n") ;
+		strncpy(r[rp].name,tok,1023) ;
+		char *colour = &finf[strlen(tok)+1] ;
+		while (isspace(*colour)) colour++ ;
+		tok = strtok(colour,",") ; red = atoi(tok) ;
+		tok = strtok(NULL,",") ; green = atoi(tok) ;
+		tok = strtok(NULL,", \n") ; blue = atoi(tok) ;
+		r[rp].colour = red<<16  | green<<8 | blue  ;
+		printf("%s => %x\t",r[rp].name,r[rp].colour) ;
+		rp++ ;
+	}
+	fclose(rf) ;
+	printf("%d ranges found\n",rp) ;
+	return rp;
+}
+
+
